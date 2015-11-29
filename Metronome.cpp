2aa90921546c19416480155a9af595f5effcfae0
Albino Tonnina <albino.tonnina@workshare.com>
2015-11-29 12:31:26 +0000

    add first bar trigger

--- Metronome.cpp
+++ Metronome.cpp
@@ -1,49 +1,50 @@
 #include "Metronome.h"
 
 void Metronome::start(float _bpm) {
-  setBPM(_bpm);
-  lastTime = (float)millis() / 1000;
-  beatsTime = 0;
+    setBPM(_bpm);
+    lastTime = (float) millis() / 1000;
+    beatsTime = 0;
+    triggerBar();
 }
 
 void Metronome::setBPM(float _bpm) {
-  bpm = _bpm;
-  bps = bpm / 60.0;
-  spb = 1.0 / bps;
+    bpm = _bpm;
+    bps = bpm / 60.0;
+    spb = 1.0 / bps;
 }
 
 void Metronome::update() {
-  float time = (float)millis() / 1000;
-  float diff = (time - lastTime) * bps;
-  lastTime = time;
+    float time = (float) millis() / 1000;
+    float diff = (time - lastTime) * bps;
+    lastTime = time;
 
-  beatsTime += diff;
+    beatsTime += diff;
 
-  beatsPassed    = floor(beatsTime);
-  barsPassed     = floor(beatsTime /  4.0);
-  sectionsPassed = floor(beatsTime / 16.0);
+    beatsPassed = floor(beatsTime);
+    barsPassed = floor(beatsTime / 4.0);
+    sectionsPassed = floor(beatsTime / 16.0);
 }
 
 bool Metronome::triggerBeat() {
-  if (beatsPassed > beatsTriggered) {
-    beatsTriggered = beatsPassed;
-    return true;
-  }
-  return false;
+    if (beatsPassed > beatsTriggered) {
+        beatsTriggered = beatsPassed;
+        return true;
+    }
+    return false;
 }
 
 bool Metronome::triggerBar() {
-  if (barsPassed > barsTriggered) {
-    barsTriggered = barsPassed;
-    return true;
-  }
-  return false;
+    if (barsPassed > barsTriggered or beatsTime == 0) {
+        barsTriggered = barsPassed;
+        return true;
+    }
+    return false;
 }
 
 bool Metronome::triggerSection() {
-  if (sectionsPassed > sectionsTriggered) {
-    sectionsTriggered = sectionsPassed;
-    return true;
-  }
-  return false;
+    if (sectionsPassed > sectionsTriggered) {
+        sectionsTriggered = sectionsPassed;
+        return true;
+    }
+    return false;
 }
